4fa736d4e974c4955e6f1c11fd3e49b79897cc7e
antoszy <michal.antoszkiewicz@gmail.com>
2018-05-21 21:41:14 +0000

    started script for converting bmp to jpg

---
+++ scripts/bmptojpg.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import glob
+import sys
+
+if __name__ == "__main__":
+	print(sys.argv);
+	if len(sys.argv)<2:
+		filelist = glob.glob("./*")
+	else:
+		filelist = glob.glob(sys.argv[1])
+
+	for f in filelist:
+		print(f)
